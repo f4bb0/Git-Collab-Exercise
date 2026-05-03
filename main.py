d390e9a52c0c1a159c78df87777c4df7922dd5b0
fabbo <farbitrarily@gmail.com>
2026-05-03 17:02:28 +0800

    Add main function for team collaboration demo

---
+++ main.py
@@ -0,0 +1,14 @@
+
+# -*- coding: utf-8 -*-
+# Git 协作练习脚本
+
+def main():
+    print("--- 团队协作演示开始 ---")
+    
+    # TODO: 请在下方添加代码
+    # 例如加一句 print
+
+    print("--- 演示结束 ---")
+
+if __name__ == "__main__":
+    main()
