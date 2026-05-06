50cf97499cc98fe908bd34c3e43cf96dbd314b26
hhp <3196794609@qq.com>
2026-05-06 21:52:05 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ def main():
     # 例如加一句 print
     print("member hhp is here!")
     print("hhp use gitbash commit this time!")
+    print("hhp use github desktop commit this time!")
     print("welcome the foreign member?")
 
     # Team member contributions all sample names
